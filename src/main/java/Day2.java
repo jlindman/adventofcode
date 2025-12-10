6c21296e1e9605b20781cb2e31acdbae0a843b2d
Jens Lindman <jens.lindman@fortra.com>
2025-12-10 10:51:37 +0100

    Day2

---
+++ src/main/java/Day2.java
@@ -0,0 +1,24 @@
+public class Day2
+{
+    static final String testInput = """
+    """;
+
+    static String input = """
+            """;
+
+    public static void main(String[] args)
+    {
+        Day2 event = new Day2();
+        event.solvePart1();
+        //event.solvePart2();
+    }
+
+    private void solvePart1()
+    {
+
+    }
+
+    private void solvePart2()
+    {
+    }
+}
